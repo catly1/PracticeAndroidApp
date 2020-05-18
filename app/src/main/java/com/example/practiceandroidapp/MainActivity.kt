0a57bfb066a108cdedcb8ecd77afe4bcc222aebd
Carlos Catly <ccatly@gmail.com>
2020-05-18 15:11:14 -0700

    basic ui

--- app/src/main/java/com/example/practiceandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/practiceandroidapp/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.practiceandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selectDateBtn.setOnClickListener { Toast.makeText(this, "button works", Toast.LENGTH_LONG).show() }
     }
 }
