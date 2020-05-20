e8f1b066a6f9d1b53ebe70bb0328bcde4fdbec8c
Carlos Catly <ccatly@gmail.com>
2020-05-19 21:04:59 -0700

    added a calendar dialog

--- app/src/main/java/com/example/practiceandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/practiceandroidapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.practiceandroidapp
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +14,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectDateBtn.setOnClickListener {
-            Toast.makeText(this, "button works", Toast.LENGTH_LONG).show()
+        selectDateBtn.setOnClickListener {view ->
+            clickDatePicker(view)
         }
+
+
+    }
+
+    fun clickDatePicker(view: View){
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener {
+                view, year, month, dayOfMonth ->
+            Toast.makeText(this, "Date picker works", Toast.LENGTH_LONG).show()
+        },year
+        ,month
+        ,day).show()
     }
 }
