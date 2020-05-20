206d42780daf89d2949b5798052e2ef9e6ca80ca
Carlos Catly <ccatly@gmail.com>
2020-05-19 23:16:30 -0700

    minutes are able to be calculated from using two dates

--- app/src/main/java/com/example/practiceandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/practiceandroidapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -27,10 +28,26 @@ class MainActivity : AppCompatActivity() {
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
         DatePickerDialog(this, DatePickerDialog.OnDateSetListener {
-                view, year, month, dayOfMonth ->
-            Toast.makeText(this, "Date picker works", Toast.LENGTH_LONG).show()
+                view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                val selectedDate = "${selectedMonth+1}/$selectedDayOfMonth/$selectedYear"
+
+
+
+
+            val dateFormat = SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH)
+
+            val formattedDate = dateFormat.parse(selectedDate)
+            val formatTest = dateFormat.format(formattedDate)
+            val formattedCurrentDate = dateFormat.format(myCalendar.time)
+
+            val currentTime = myCalendar.time.time;
+            val calculatedTimeInMinutes = (currentTime - formattedDate.time) / 1000 / 60
+            Toast.makeText(this, "The selected date is ${formatTest} and current time is $formattedCurrentDate", Toast.LENGTH_LONG).show()
+            selectedDateView.setText(selectedDate)
+            ageInMinutes.setText(calculatedTimeInMinutes.toString())
         },year
         ,month
         ,day).show()
     }
+
 }
